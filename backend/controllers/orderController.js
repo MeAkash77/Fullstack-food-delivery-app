273168715889c249d9f8789b0965ca62e3ddf131
Me Akash <158308964+MeAkash77@users.noreply.github.com>
2025-04-05 20:48:35 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 // Placing user order for frontend
 const placeOrder = async (req, res) =>{
 
-    const frontend_url = 'http://localhost:5173';
+    const frontend_url = 'https://fullstack-food-frontend-2id7.onrender.com/cart';
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
